0a124836a4404ed4ccfca31909e8a5f400f5f90e
YesicaY <yesicay.mesai@unac.edu.co>
2026-09-14 15:22:35 -0500

    Se corrige padding

--- app/src/main/java/com/example/miappparcial/MainActivity.kt
+++ app/src/main/java/com/example/miappparcial/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -19,10 +21,12 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     var nombreRegistrado by remember { mutableStateOf<String?>(null) }
 
-                    if (nombreRegistrado == null) {
-                        RegistrationScreen(onRegistrado = { nombre -> nombreRegistrado = nombre })
-                    } else {
-                        MainTabsScreen(nombre = nombreRegistrado!!)
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        if (nombreRegistrado == null) {
+                            RegistrationScreen(onRegistrado = { nombre -> nombreRegistrado = nombre })
+                        } else {
+                            MainTabsScreen(nombre = nombreRegistrado!!)
+                        }
                     }
                 }
             }
